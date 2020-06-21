edb506e7bb869b8dce0b0f3e6eea4bc17d456944
innfi <innfi.world@gmail.com>
2020-06-21 19:44:43 +0900

    https://www.codewars.com/kata/5626b561280a42ecc50000d1

--- javascript/200620-ConsecutivePower.js
+++ javascript/200620-ConsecutivePower.js
@@ -2,7 +2,19 @@
 
 https://www.codewars.com/kata/5626b561280a42ecc50000d1
 
+one of the optimal solutions was: 
 
+function filterFunc(n) {
+  return `${n}`.split("").map((x, i) => x ** (i+1)).reduce((a, b) => a+b) == n;
+}
+
+function *range(a, b) {
+  for (var i = a; i <= b; ++i) yield i;
+}
+
+function sumDigPow(a, b) {
+  return Array.from(range(a, b)).filter(filterFunc);
+}
 */
 
 const assert = require('assert');
@@ -10,7 +22,13 @@ const assert = require('assert');
 const dummyResult = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 sumDigPow = (start, end) => {
-    return dummyResult;
+	let resultArray = [];
+	
+	for(let i=start;i<=end;i++) {
+		if(isMagicNumber(i)) resultArray.push(i);
+	}
+
+	return resultArray;
 }
 
 isMagicNumber = (number) => {
@@ -28,12 +46,23 @@ isMagicNumber = (number) => {
 
 describe('consecutive power numbers', () => {
     it('shaping the function', () => {
-        assert.equal(sumDigPow(1, 10), dummyResult);
+        assert.deepEqual(sumDigPow(1, 10), dummyResult);
     });
 
 	it('tells a candidate is a magic number', () => {
 		assert.equal(isMagicNumber(89), true);
-
 		assert.equal(isMagicNumber(88), false);
+	});
+
+	it('tests example case 1', () => {
+		assert.deepEqual(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+	});
+
+	it('tests example case 2', () => {
+		assert.deepEqual(sumDigPow(90, 100), []);
+	});
+
+	it('tests example case 3', () => {
+		assert.deepEqual(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]);
 	});
 });
