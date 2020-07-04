f7b3cb4b6f657f64ddf9cc0a0b959c7b0eabe52b
innfi <innfi.world@gmail.com>
2020-07-04 21:58:18 +0900

    https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c

--- javascript/200626-MaxSubarraySum.js
+++ javascript/200626-MaxSubarraySum.js
@@ -1,24 +1,43 @@
 /*
 https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
 
+optimal solution: 
+
+var maxSequence = function(arr){
+  var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+}
+
 */
 
 const assert = require('assert');
 
-
 var maxSequence = (arr) => {
-    return [ 0 ];
+    let maxSum = 0;
+
+    for(let i=0;i<arr.length;i++) {
+        let partialSum = partialMaxSum(arr, i);
+        if(partialSum > maxSum) maxSum = partialSum;
+    }
+
+    return maxSum;
 }
 
 var partialMaxSum = (arr, index) => {
-    let sumArray = [];
+    var maxNumber = 0;
 
-    for(let i=0;i<arr.length-index;i++) {
+    for(let i=index;i<arr.length;i++) {
         const subArray = arr.slice(index, i+1);
-        sumArray.push(subArray.reduce((acc, cur) => acc + cur));
+        const partialSum = subArray.reduce((acc, cur) => acc + cur);
+        if(partialSum > maxNumber) maxNumber = partialSum;
     }
 
-    return sumArray;
+    return maxNumber;
 }
 
 describe('maximum subarray sum', () => {    
@@ -28,21 +47,17 @@ describe('maximum subarray sum', () => {
         assert.equal(maxSequence([]), 0);
     });
 
-    // it('test case 1', () => {
-    //     assert.equal(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
-    // });
-
-    // it('test subroutine case 1', () => {
-    //     assert.equal(partialMaxSum([-2, 1, -3, 4, -1, 2, 1, -5, 4], 0), 0);
-    // });
-
-    it('tests array.slice', () => {
-        assert.deepEqual(testArray.slice(0, 1), [-2]);
-        assert.deepEqual(testArray.slice(0, 2), [-2, 1]);
+    it('tests partial sum', () => {
+        assert.equal(partialMaxSum(testArray, 0), 2);
     });
 
-    it('tests index', () => {
-        assert.deepEqual(partialMaxSum(testArray, 0), 
-            [-2, -1, -4, 0, -1, 1, 2, -3, 1]);
+    it('tests slice for small arrays', () => {
+        const testArray1 = [1, 2, 3];
+
+        assert.deepEqual(testArray1.slice(1, 3), [2, 3]);
+    });
+
+    it('tests maximum partial sum', () => {
+        assert.equal(maxSequence(testArray), 6);
     });
 });
