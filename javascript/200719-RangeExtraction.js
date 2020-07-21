5581f1743096af7e0e84c4b19db8bada0bcc2a77
innfi <innfi.world@gmail.com>
2020-07-21 22:41:41 +0900

    https://www.codewars.com/kata/51ba717bb08c1cd60f00002f (before refactoring)

--- javascript/200719-RangeExtraction.js
+++ javascript/200719-RangeExtraction.js
@@ -5,69 +5,52 @@ https://www.codewars.com/kata/51ba717bb08c1cd60f00002f
 
 const assert = require('assert');
 
-class Stack {
-    constructor() {
-        this.data = [];
-    }
-
-    push(element) {
-        this.data.push(element);
-    }
-
-    front() {
-        return this.data[this.data.length-1];
-    }
-
-    pop() {
-        return this.data.pop();
-    }
-}
-
 let solution = (list) => {
-    return "-6,-3-1,3-5,7-11,14,15,17-20";
-}
+    let resultString = "";
+    let queue = [];
 
-let partialSolution = (list) => {
-    let firstNumber = null;
-    let lastNumber = null;
-
-    for(let i=0;i<list.length-2;i++) {
-        if(firstNumber == null) {
-            firstNumber = list[i];
+    list.forEach((element) => {
+        if(queue.length <= 0 || queue[queue.length-1] + 1 === element) {
+            queue.push(element);
+            return;
         }
 
-        if(list[i] + 1 === list[i+1] && list[i+1] + 1 === list[i+2]) {
-            lastNumber = list[i+2];
+        if(queue.length >= 3) {
+            resultString += queue[0].toString() + "-" + queue[queue.length-1].toString() + ",";
+        } else {
+            queue.forEach((queueElement) => {
+                resultString += queueElement.toString() + ",";
+            });
         }
+
+        queue = [];
+        queue.push(element);
+    });
+
+    if(queue.length >= 3) {
+        resultString += queue[0].toString() + "-" + queue[queue.length-1].toString() + ",";
+    } else {
+        queue.forEach((queueElement) => {
+            resultString += queueElement.toString() + ",";
+        });
     }
 
-    return firstNumber.toString() + "-" + lastNumber.toString();
+    return resultString.slice(0, -1);
 }
 
 describe('range extraction', () => {
-    it('tests stack', () => {
-        const stack = new Stack();
-        stack.push(1);
-        stack.push(3);
-        stack.push(5);
-
-        assert.equal(stack.front(), 5);
-        stack.pop();
-        assert.equal(stack.front(), 3);
+    it('tests partial range', () => {
+        assert.equal(solution([1, 2, 3]), "1-3");
     });
 
-    //it('tests partial range', () => {
-    //    assert.equal(partialSolution([1, 2, 3]), "1-3");
-    //});
+    it('tests non-consecutive cases', () => {
+        assert.equal(solution([1, 3, 5]), "1,3,5");
+    });
 
-    //it('tests non-consecutive cases', () => {
-    //    assert.equal(partialSolution([1, 3, 5]), "1,3,5");
-    //});
+    it('tests case 1', () => {
+        const input = [-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20];
+        const result = "-6,-3-1,3-5,7-11,14,15,17-20";
 
-    //it('tests case 1', () => {
-    //    const input = [-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20];
-    //    const result = "-6,-3-1,3-5,7-11,14,15,17-20";
-
-    //    assert.equal(solution(input), result);
-    //});
+        assert.equal(solution(input), result);
+    });
 });
