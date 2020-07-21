a29bf0e7bbe7a65d6d116a4473ab3537f23aad5c
innfi <innfi.world@gmail.com>
2020-07-21 23:12:08 +0900

    https://www.codewars.com/kata/51ba717bb08c1cd60f00002f (refactored)

--- javascript/200719-RangeExtraction.js
+++ javascript/200719-RangeExtraction.js
@@ -1,44 +1,153 @@
 /*
 https://www.codewars.com/kata/51ba717bb08c1cd60f00002f
 
+one of the optimal solutions was : 
+
+function solution(list){
+   return list
+     .reduce((acc, n, i, arr) => {
+       if(n !== arr[i - 1] + 1) {
+         acc.push([n]);
+       } else {
+         acc[acc.length - 1].push(n);
+       }
+       return acc;
+     }, [])
+     .map(arr => arr.length > 2 ? `${arr[0]}-${arr[arr.length-1]}` : arr)
+     .join(',');
+}
+
 */
 
 const assert = require('assert');
 
+class RangeQueue {
+    constructor() {
+        this.data = [];
+    }
+
+    enque(element) {
+        this.data.push(element);
+    }
+
+    start() {
+        return this.data[0];
+    }
+
+    end() {
+        return this.data[this.data.length-1];
+    }
+
+    empty() {
+        return this.data.length <= 0;
+    }
+
+    clear() {
+        this.data = [];
+    }
+
+    isContiguous(element) {
+        return this.end() + 1 === element;
+    }
+
+    toString() {
+        if(this.data.length >= 3) {
+            return this.start().toString() + "-" + this.end().toString() + ",";
+        } 
+
+        let resultString = "";
+        this.data.forEach((queueElement) => {
+            resultString += queueElement.toString() + ",";
+        });
+
+        return resultString;
+    }
+}
+
 let solution = (list) => {
     let resultString = "";
-    let queue = [];
+    const queue = new RangeQueue();
 
     list.forEach((element) => {
-        if(queue.length <= 0 || queue[queue.length-1] + 1 === element) {
-            queue.push(element);
+        if(queue.empty() || queue.isContiguous(element) == true) {
+            queue.enque(element);
             return;
         }
 
-        if(queue.length >= 3) {
-            resultString += queue[0].toString() + "-" + queue[queue.length-1].toString() + ",";
-        } else {
-            queue.forEach((queueElement) => {
-                resultString += queueElement.toString() + ",";
-            });
-        }
-
-        queue = [];
-        queue.push(element);
+        resultString += queue.toString();
+        
+        queue.clear();
+        queue.enque(element);
     });
 
-    if(queue.length >= 3) {
-        resultString += queue[0].toString() + "-" + queue[queue.length-1].toString() + ",";
-    } else {
-        queue.forEach((queueElement) => {
-            resultString += queueElement.toString() + ",";
-        });
-    }
-
-    return resultString.slice(0, -1);
+    return (resultString + queue.toString()).slice(0, -1);
 }
 
 describe('range extraction', () => {
+    it('tests queue start', () => {
+        const queue = new RangeQueue();
+        queue.enque(1);
+        queue.enque(2);
+        assert.equal(queue.start(), 1);
+
+        queue.enque(3);
+        assert.equal(queue.start(), 1);
+    });
+
+    it('tests queue end', () => {
+        const queue = new RangeQueue();
+        queue.enque(1);
+        queue.enque(2);
+        assert.equal(queue.end(), 2);
+
+        queue.enque(3);
+        assert.equal(queue.end(), 3);
+    });
+
+    it('tests queue empty', () => {
+        const queue = new RangeQueue();
+        assert.equal(queue.empty(), true);
+        queue.enque(1);
+        assert.equal(queue.empty(), false);
+    });
+
+    it('tests queue clear', () => {
+        const queue = new RangeQueue();
+        queue.enque(1);
+        queue.enque(2);
+        queue.enque(3);
+        queue.clear();
+        assert.equal(queue.empty(), true);
+    });
+
+    it('tests queue isContiguous', () => {
+        const queue = new RangeQueue();
+        queue.enque(1);
+        assert.equal(queue.isContiguous(2), true);
+        assert.equal(queue.isContiguous(5), false);
+
+        queue.enque(2);
+        assert.equal(queue.isContiguous(2), false);
+        assert.equal(queue.isContiguous(3), true);
+    });
+
+    it('tests queue toString for contiguous elements', () => {
+        const queue = new RangeQueue();
+        queue.enque(1);
+        queue.enque(2);
+        queue.enque(3);
+
+        assert.equal(queue.toString(), "1-3,");
+    });
+
+    it('tests queue toString for not contiguous enough elements case', () => {
+        const queue = new RangeQueue();
+        queue.enque(9);
+        queue.enque(10);
+
+        assert.equal(queue.toString(), "9,10,");
+    });
+
     it('tests partial range', () => {
         assert.equal(solution([1, 2, 3]), "1-3");
     });
