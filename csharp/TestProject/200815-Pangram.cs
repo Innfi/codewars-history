3ac901a60a9c94867225cc2b70f19da89c527cbd
innfi <innfi.world@gmail.com>
2020-08-15 22:22:56 +0900

    https://www.codewars.com/kata/52b757663a95b11b3d00062d https://www.codewars.com/kata/545cedaa9943f7fe7b000048

---
+++ csharp/TestProject/200815-Pangram.cs
@@ -0,0 +1,43 @@
+﻿/*
+ https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+ */
+
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Pangram
+{
+    public class Kata
+    {
+        public static bool IsPangram(string str)
+        {
+            var letterDict = new bool['z'-'a'+1];
+
+            foreach (var letter in str.ToLower())
+            {
+                if (char.IsLetter(letter)) letterDict[(int)letter-'a'] = true;
+            }
+
+            foreach (var item in letterDict)
+            {
+                if (item == false) return false;
+            }
+
+            return true;
+        }
+    }
+
+    [TestClass]
+    public class TestPangram
+    {        
+        public string text = "The quick brown fox jumps over the lazy dog.";
+
+        [TestMethod]
+        public void Test1SimpleCase()
+        {
+            Assert.AreEqual(Kata.IsPangram(text), true);
+        }
+    }
+}
---
+++ csharp/TestProject/200815-WeirdStringCase.cs
@@ -0,0 +1,58 @@
+﻿/*
+ https://www.codewars.com/kata/52b757663a95b11b3d00062d
+
+ */
+
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace WeirdStringCase
+{
+    public class Kata
+    {
+        public static string ToWeirdCase(string s)
+        {
+            return s.Split(' ').Select((token) => {
+                return new string(token.Select((letter, index) => {
+                    if (index % 2 == 1) return char.ToLower(letter);
+                    else return char.ToUpper(letter);
+                }).ToArray());
+            }).Aggregate((i, j) => i + " " + j);
+        }
+    }
+
+    [TestClass]
+    public class TestWeirdStringCase
+    {
+        [TestMethod]
+        public void Test0Split()
+        {
+            var text = "this is a string";
+            var tokens = text.Split(' ').ToList();
+
+            var result = tokens.Aggregate((i, j) => i + " " + j);
+
+            Assert.AreEqual(text, result);
+        }
+
+        [TestMethod]
+        public void Test0Select()
+        {
+            var text = "string";
+            var result = new string(text.Select((letter, index) => {
+                if (index % 2 == 1) return char.ToLower(letter);
+                else return char.ToUpper(letter);
+            }).ToArray());
+
+            Assert.AreEqual(result, "StRiNg");
+        }
+
+        [TestMethod]
+        public void Test1SimpleCase()
+        {
+            Assert.AreEqual(Kata.ToWeirdCase("String"), "StRiNg");
+            Assert.AreEqual(Kata.ToWeirdCase("This is a test"), "ThIs Is A TeSt");
+        }
+    }
+}
