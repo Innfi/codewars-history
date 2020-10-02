bd36c5a987987409daa658ac947dc5b3b4a18189
innfi <innfi.world@gmail.com>
2020-10-02 23:42:45 +0900

    https://www.codewars.com/kata/54b42f9314d9229fd6000d9c (ongoing)

--- typescript/201001-DuplicateEncoder.ts
+++ typescript/201001-DuplicateEncoder.ts
@@ -4,6 +4,17 @@
 
 import assert from 'assert';
 
+const duplicateEncode = (word: string): string {
+    let result: string = '';
+
+    for(const letter of word.toLowerCase().split('')) {
+        const re = new RegExp(letter, 'g');
+        const count: number = (word.match(re) || []).length;
+
+        if(count > 1) result += ')';
+        else result += '(';
+    }
+} 
 
 describe('Building blocks', () => {
     it('get character occurrences', () => {
@@ -12,6 +23,22 @@ describe('Building blocks', () => {
         const count: number = (testString.match(/a/g) || []).length;
 
         assert.strictEqual(count, 4);
+    });
+
+    it('bruteforce approach', () => {
+        const testString: string = 'qawaeara';
+
+        let result: string = '';
+
+        for(const letter of testString.split('')) {
+            const re = new RegExp(letter, 'g');
+            const count: number = (testString.match(re) || []).length;
+
+            if(count > 1) result += ')';
+            else result += '(';
+        }
+
+        assert.strictEqual(result, '()()()()');
     });
 });
 
