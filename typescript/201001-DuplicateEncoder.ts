072abef999c501507fe032fe3384a2590bbaf271
innfi <innfi.world@gmail.com>
2020-10-03 19:20:27 +0900

    https://www.codewars.com/kata/54b42f9314d9229fd6000d9c

--- typescript/201001-DuplicateEncoder.ts
+++ typescript/201001-DuplicateEncoder.ts
@@ -1,10 +1,41 @@
 /**
  *  https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+ * 
+ *export function duplicateEncode(word: string){
+        // ...
+        return word
+        .toLowerCase()
+        .split('')
+        .map((a, i, w) => {
+          return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+        })
+        .join('')
+    }
  */
 
 import assert from 'assert';
 
-const duplicateEncode = (word: string): string {
+const duplicateEncode = (word: string): string => {
+    let result: string = '';
+    interface Dictionary<T> { [key: string]: T; }
+    let letterDict: Dictionary<number> = {};
+
+    const wordArray = word.toLowerCase().split('');
+    for(const letter of wordArray) {
+        if(letterDict[letter] === undefined) letterDict[letter] = 0;
+
+        letterDict[letter] += 1;
+    }
+
+    for(const letter of wordArray) {
+        if(letterDict[letter] > 1) result += ')';
+        else result += '(';
+    }
+
+    return result;
+}
+
+const duplicateEncodeBackup = (word: string): string => {
     let result: string = '';
 
     for(const letter of word.toLowerCase().split('')) {
@@ -14,6 +45,8 @@ const duplicateEncode = (word: string): string {
         if(count > 1) result += ')';
         else result += '(';
     }
+
+    return result;
 } 
 
 describe('Building blocks', () => {
@@ -40,10 +73,19 @@ describe('Building blocks', () => {
 
         assert.strictEqual(result, '()()()()');
     });
+
+    it('dictionary edge case', () => {
+        interface Dictionary<T> { [key: string]: T; }
+        let letterDict: Dictionary<number> = {};
+       
+        assert.strictEqual(letterDict['notexist'], undefined);
+    });
 });
 
-//describe('Duplicate Encoder', () => {
-//
-//});
+describe('Duplicate Encoder', () => {
+    it('test case', () => {
+        assert.strictEqual(duplicateEncode('(( @)'), '))(((');
+    });
+});
 
 
