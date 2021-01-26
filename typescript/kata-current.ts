f48a702b3dc117e2f982c0eca05eca1686915ad2
innfi <innfi.world@gmail.com>
2021-01-26 21:55:17 +0900

    https://www.codewars.com/kata/56a5d994ac971f1ac500003e

--- typescript/kata-current.ts
+++ typescript/kata-current.ts
@@ -1,34 +1,8 @@
 /**
-https://www.codewars.com/kata/56a5d994ac971f1ac500003e
  */
 
 import assert from 'assert';
 
-const longest = (input: string[], k: number): string => {
-    return "";
-};
-
 describe('Building blocks', () => {
-    const input: string[] = ["zone", "abigail", "theta", "form", "libe", "zas"];
-    it('array slice', () => {
-        assert.deepStrictEqual(input.slice(0, 2), ["zone", "abigail"]);
-        assert.deepStrictEqual(input.slice(2, 4), ["theta", "form"]);
-    });
 
-    it('make consecutive string', () => {
-        assert.strictEqual(input.slice(0, 3).join(''), 'zoneabigailtheta');
-    });
-});
-
-
-describe('Consecutive strings', () => {
-    //it('test case', () => {
-    //    assert.strictEqual(
-    //        longest(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta");
-    //    assert.strictEqual(
-    //        longest(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", 
-    //        "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh");
-    //    assert.strictEqual(longest(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), 
-    //        "ixoyx3452zzzzzzzzzzzz");
-    //});
 });
