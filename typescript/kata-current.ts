ad5744e149840049848ba79190c39ab6c87c082c
innfi <innfi.world@gmail.com>
2021-02-10 23:58:25 +0900

    https://www.codewars.com/kata/5270f22f862516c686000161 (ongoing)

--- typescript/kata-current.ts
+++ typescript/kata-current.ts
@@ -5,7 +5,16 @@
 import assert from 'assert';
 
 const toBase64 = (str: string): string => {
-    return '';
+    const letters: string = 
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+    return str
+        .split('')
+        .map((value: string) => value.charCodeAt(0).toString(2).padStart(8, '0'))
+        .join('')
+        .match(/.{1,6}/g)
+        .map((value: string) => letters.charAt(parseInt(value, 2)) )
+        .join('');
 };
 
 const fromBase64 = (str: string): string => {
@@ -32,15 +41,75 @@ describe('Building blocks', () => {
 
         assert.strictEqual(String.fromCharCode(parseInt(input, 2)), 'M');
     });
+
+    it('cut bytes by 6', () => {
+        const input: string = 'Man';
+
+        const converted: string = input
+        .split('')
+        .map((value: string) => value.charCodeAt(0).toString(2).padStart(8, '0'))
+        .join('')
+        .match(/.{1,6}/g)
+        .map((value: string) => letters.charAt(parseInt(value, 2)) )
+        .join('');
+       
+        assert.strictEqual(converted, 'TWFu');
+    });
+
+    it('test filter', () => {
+        const input: string = 'test==';
+
+        console.log('test: ', input.split('='));
+    });
+
+    it('reverse', () => {
+        const input: string = 'TWFu=';
+
+        const converted = input.split('=')[0]
+        .split('')
+        .map((value: string) => letters.indexOf(value).toString(2).padStart(6, '0'))
+        .join('')
+        .match(/.{1,8}/g)
+        .map((value: string) => {
+            const test: number = parseInt(value, 2);
+            console.log('test: ', test);
+
+            return value.charCodeAt(parseInt(value, 2));
+        })
+        .join('');
+
+        //assert.strictEqual(converted, 'Man');
+    });
 });
 
 describe('Base64', () => {
-    //it('simple case', () => {
-    //    assert.strictEqual(toBase64('MAN'), 'TWFu');
-    //});
+    const letters: string = 
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    it('simple case', () => {
+        assert.strictEqual(toBase64('Man'), 'TWFu');
+    });
 
-    //it('test case', () => {
-    //    assert.strictEqual(toBase64('this is a string!!'), 'dGhpcyBpcyBhIHN0cmluZyEh');
+    it('paddings', () => {
+        const input: string = 'Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.';
+        const output: string = 'TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=';
+
+        const converted: string = input
+        .split('')
+        .map((value: string) => value.charCodeAt(0).toString(2).padStart(8, '0'))
+        .join('')
+        .match(/.{1,6}/g)
+        .map((value: string, index: number, array: string[]) => {
+            return letters
+            .charAt(parseInt(value.padEnd(6, '0'), 2))
+            .concat('='.repeat((6-value.length)/2));
+        })
+        .join('');
+
+        assert.strictEqual(output, converted);
+    });
+
+    it('test case', () => {
+        assert.strictEqual(toBase64('this is a string!!'), 'dGhpcyBpcyBhIHN0cmluZyEh');
     //    assert.strictEqual(fromBase64('dGhpcyBpcyBhIHN0cmluZyEh'), 'this is a string!!');
-    //});
+    });
 });
