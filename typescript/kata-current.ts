ba63ab668ec84b87d830bb533ac792d4cc902369
innfi <innfi.world@gmail.com>
2021-03-20 21:40:25 +0900

    https://www.codewars.com/kata/539a0e4d85e3425cb0000a88 (skipped)

--- typescript/kata-current.ts
+++ typescript/kata-current.ts
@@ -1,23 +1,8 @@
 /**
- * https://www.codewars.com/kata/539a0e4d85e3425cb0000a88
  */
 
 import assert from 'assert';
 
-function add(x: number): any {
-    console.log('args: ', arguments);
-    return function(y: number) {
-        console.log('args: ', arguments);
-        return x+y;
-    };
-};
 
 describe('Building blocks', () => {
-
-});
-
-describe('ChainAdding', () => {
-    it('test case', () => {
-        //assert.strictEqual(add(1)(2)(3)(4)(5), 15);
-    });
 });
