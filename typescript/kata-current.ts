1863f26cc7a5534a03c18fc0f9d6758e2680d5c9
innfi <innfi.world@gmail.com>
2021-04-09 22:33:04 +0900

    https://www.codewars.com/kata/56dbf59b0a10feb08c000227 (ongoing)

--- typescript/kata-current.ts
+++ typescript/kata-current.ts
@@ -21,6 +21,20 @@ function oper(fct: (s: string) => string, s: string): string {
     return fct(s);
 }
 
+const toMatrix = (input: string): string[][] => {
+    return input.split('\n').map((value: string) => value.split(''));
+};
+
+const doReverse = (input: string[][]): string[][] => {
+    return input.map((value: string[]) => value.reverse());
+};
+
+const doTranspose = (input: string[][]): string[][] => {
+    return Object.keys(input[0]).map((column: string) => {
+        return input.map((row: string[]) => row[column]);
+    });
+};
+
 describe('Building blocks', () => {
     it('split input into arrays', () => {
         const input: string = 'EcGcXJ\naaygcA\nNgIshN\nyOrCZE\neBEqpm\nNkxCgw';
@@ -45,6 +59,24 @@ describe('Building blocks', () => {
         testOutput.push(newRowNext);
 
         assert.deepStrictEqual(testOutput, [['a','c'], ['b','d']]);
+    });
+
+    it('array reverse', () => {
+        assert.deepStrictEqual([1, 2, 3].reverse(), [3, 2, 1]);
+
+        const input: string[][] = [['a','b','c'], ['d','e','f'], ['g','h','i']];
+
+        const output: string[][] = input.map((value: string[]) => value.reverse());
+        assert.deepStrictEqual(output, [['c','b','a'],['f','e','d'],['i','h','g']]);
+    });
+
+    it('array transpose', () => {
+        const input: number[][] = [[1,2], [3,4]];
+        const output: number[][] = Object.keys(input[0]).map((column: string) => {
+            return input.map((row: number[]) => row[column]);
+        });
+
+        assert.deepStrictEqual(output, [[1,3],[2,4]]);
     });
 });
 
