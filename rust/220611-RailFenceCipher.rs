ec5207cd1258dfc44922c19eebfef4d9701a6b7f
innfi <innfi.world@gmail.com>
2022-06-12 18:48:18 +0900

    https://www.codewars.com/kata/58c5577d61aefcf3ff000081

--- rust/220611-RailFenceCipher.rs
+++ rust/220611-RailFenceCipher.rs
@@ -75,10 +75,63 @@ mod tests {
                 }).collect()
         }).collect::<Vec<String>>().join("")
     }
-    
-    // fn decode_rail_fence_cipher(text: &str, num_rails: usize) -> String {
-        
-    // }
+
+    #[test]
+    fn test_decoding() {
+        let input: &str = "WECRLTEERDSOEEFEAOCAIVDEN";
+        let bucket_count = 3;
+        let pendulum_indexes = pendulum(input.len(), bucket_count);
+
+        let mut start = 0;
+        let mut output_vec: Vec<(usize, char)> = Vec::new();
+
+        (0..bucket_count).for_each(|i| {
+            let filtered = pendulum_indexes.iter().filter(|x| x.1 == i as i32)
+                .map(|(pi, bucket)| (*pi, *bucket))
+                .collect::<Vec<(usize, i32)>>();
+            let end = start + filtered.len();
+            let substring = input.get(start..end).unwrap();
+
+            substring.chars().enumerate().for_each(|(si, c)| {
+                output_vec.push((filtered[si].0, c));
+            });
+
+            start = end;
+        });
+
+        assert_eq!(output_vec.len(), input.len());
+
+        output_vec.sort_by(|a, b| a.0.cmp(&b.0));
+
+        let out_string: String = output_vec.into_iter().map(|x| x.1).collect();
+        assert_eq!(out_string, String::from("WEAREDISCOVEREDFLEEATONCE"));
+    }
+
+    fn decode_rail_fence_cipher(text: &str, num_rails: usize) -> String {
+        let pendulum_indexes = pendulum(text.len(), num_rails);
+
+        let mut start = 0;
+        let mut output_vec: Vec<(usize, char)> = Vec::new();
+
+        (0..num_rails).for_each(|rail_index| {
+            let filtered_indexes = pendulum_indexes.iter()
+                .filter(|x| x.1 == rail_index as i32)
+                .map(|(pi, bucket)| (*pi, *bucket))
+                .collect::<Vec<(usize, i32)>>();
+            let end = start + filtered_indexes.len();
+            
+            let substring = text.get(start..end).unwrap();
+            substring.chars().enumerate().for_each(|(si, c)| {
+                output_vec.push((filtered_indexes[si].0, c));
+            });
+
+            start = end;
+        });
+
+        output_vec.sort_by(|a, b| a.0.cmp(&b.0));
+
+        output_vec.into_iter().map(|x| x.1).collect()
+    }
 
     #[test]
     fn basic_tests() {
@@ -86,17 +139,17 @@ mod tests {
             encode_rail_fence_cipher("WEAREDISCOVEREDFLEEATONCE", 3),
             "WECRLTEERDSOEEFEAOCAIVDEN"
         );
-        // assert_eq!(
-        //     decode_rail_fence_cipher("WECRLTEERDSOEEFEAOCAIVDEN", 3),
-        //     "WEAREDISCOVEREDFLEEATONCE"
-        // );
+        assert_eq!(
+            decode_rail_fence_cipher("WECRLTEERDSOEEFEAOCAIVDEN", 3),
+            "WEAREDISCOVEREDFLEEATONCE"
+        );
         assert_eq!(
             encode_rail_fence_cipher("Hello, World!", 3),
             "Hoo!el,Wrdl l"
         );
-        // assert_eq!(
-        //     decode_rail_fence_cipher("Hoo!el,Wrdl l", 3),
-        //     "Hello, World!"
-        // );
+        assert_eq!(
+            decode_rail_fence_cipher("Hoo!el,Wrdl l", 3),
+            "Hello, World!"
+        );
     }
 }
