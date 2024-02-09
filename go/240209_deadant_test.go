6d21e7891f6ba357897f0337d75ea61c08f340dd
innfi <innfi.world@gmail.com>
2024-02-09 22:02:24 +0900

    https://www.codewars.com/kata/57d5e850bfcdc545870000b7

---
+++ go/240209_deadant_test.go
@@ -0,0 +1,46 @@
+// https://www.codewars.com/kata/57d5e850bfcdc545870000b7
+
+package codewars_history
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func DeadAntCount(ants string) int {
+	withoutAnts := strings.Replace(ants, "ant", " ", -1)
+	reg := regexp.MustCompile(`[a|n|t]+`)
+
+	debris := reg.FindAllString(withoutAnts, -1)
+	fmt.Println("debris: ", debris)
+	return len(debris)
+}
+
+func TestInit(t *testing.T) {
+	fmt.Println("testinit")
+
+	input := "ant anantt aantnt"
+	output := strings.Replace(input, "ant", " ", -1)
+	assert.Equal(t, output, "  an t a nt")
+
+	reg := regexp.MustCompile(`\s+`)
+	afterRegex := reg.ReplaceAllString(output, " ")
+	assert.Equal(t, afterRegex, " an t a nt")
+
+	splitted := strings.Split(afterRegex, " ")
+	for _, elem := range splitted {
+		fmt.Println("elem: ", elem)
+	}
+
+	assert.Equal(t, len(splitted), 5)
+}
+
+func TestCase(t *testing.T) {
+	input := "t.nt.nt.a..ntan.a....aa.t..na...tantt.anantan..ta.nnt"
+
+	assert.Equal(t, DeadAntCount(input), 9)
+}
