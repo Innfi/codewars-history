2fcce73c34b61dcf35f0315246403a36df93c80a
innfi <innfi.world@gmail.com>
2024-03-13 21:31:45 +0900

    https://www.codewars.com/kata/60cc93db4ab0ae0026761232

--- rust/2024/240312-BackAndForth.rs
+++ rust/2024/240312-BackAndForth.rs
@@ -1,5 +1,15 @@
 // https://www.codewars.com/kata/60cc93db4ab0ae0026761232
 
+/**
+ * fn arrange(s: &[i32]) -> Vec<i32> {
+    (0..s.len())
+        .map(|i| match i % 4 {
+            0 | 3 => s[i / 2],
+            _ => s[s.len() - i / 2 - 1],
+        })
+        .collect()
+}
+ */
 #[cfg(test)]
 mod tests {
   #[derive(PartialEq)]
@@ -21,7 +31,7 @@ mod tests {
         current: 0,
         head: 0,
         tail: len-1,
-        state: IndexerState::Head,
+        state: IndexerState::AfterHead,
       }
     }
 
@@ -44,7 +54,9 @@ mod tests {
       } else if self.state == IndexerState::BeforeHead {
         let output = self.current;
 
+        self.tail -= 1;
         self.head += 1;
+
         self.current = self.head;
         self.state = IndexerState::Head;
 
@@ -69,23 +81,32 @@ mod tests {
     assert_eq!(instance.next(), 5);
     assert_eq!(instance.next(), 4);
     assert_eq!(instance.next(), 1);
-    assert_eq!(instance.next(), 0);
-    assert_eq!(instance.next(), 0);
+    assert_eq!(instance.next(), 2);
+    assert_eq!(instance.next(), 3);
   }
 
-  // fn arrange(s: &[i32]) -> Vec<i32> {
-  //   // swap and take two?
+  fn arrange(s: &[i32]) -> Vec<i32> {
+    let mut output: Vec<i32> = Vec::new();
+    if s.len() <= 0 {
+      return output;
+    }
 
-  //   todo!()
-  // }
+    let mut indexer = Indexer::new(s.len());
 
-  // #[test]
-  // fn returns_expected() {
-  //   assert_eq(arrange(vec![1]), vec![1]);
-  //   assert_eq(arrange(vec![]), vec![]);
-  //   assert_eq(arrange(vec![1,2]), vec![1,2]);
-  //   assert_eq(arrange(vec![4, 3, 2]), vec![4, 2, 3]);
-  //   assert_eq(arrange(vec![2, 4, 3, 4]), vec![2, 4, 3, 4]);
-  //   assert_eq(arrange(vec![9, 7, -2, 8, 5, -3, 6, 5, 1]), vec![9, 1, 5, 7, -2, 6, -3, 8, 5]);
-  // }
+    for _ in 0..s.len() {
+      output.push(s[indexer.next()]);
+    }
+
+    output
+  }
+
+  #[test]
+  fn returns_expected() {
+    assert_eq!(arrange(&[9, 7, -2, 8, 5, -3, 6, 5, 1]), vec![9, 1, 5, 7, -2, 6, -3, 8, 5]);
+    assert_eq!(arrange(&[1]), vec![1]);
+    assert_eq!(arrange(&[]), vec![]);
+    assert_eq!(arrange(&[1,2]), vec![1,2]);
+    assert_eq!(arrange(&[4, 3, 2]), vec![4, 2, 3]);
+    assert_eq!(arrange(&[2, 4, 3, 4]), vec![2, 4, 3, 4]);
+  }
 }
